89dd6f8ba21befccffbe3f3d429c4b2ee0d6b434
SniffbI4 <serzh.better@yandex.ru>
2022-04-02 14:34:43 +0300

    Подсветка предметов при приближении и появление текста
    Повторно импортирован TextMeshPro. В основной ветке он же есть

--- Assets/Sergey_Develop/Quest/PErsonMove.cs
+++ Assets/Sergey_Develop/Quest/PErsonMove.cs
@@ -5,15 +5,40 @@ using UnityEngine;
 public class PErsonMove : MonoBehaviour
 {
     public float speed = 2f;
+    private Vector3 target;
+
+    private void Start()
+    {
+        target = transform.position;
+    }
 
     private void Update()
     {
-        float input = Input.GetAxis("Horizontal");
-
-        if (Mathf.Abs(input)>0.1f)
+        if (Input.GetMouseButtonDown(0))
         {
-            Vector2 newPos = new Vector2(transform.position.x + input * speed * Time.deltaTime, transform.position.y);
-            transform.position = newPos;
+            SetNewPosition();
         }
+
+        transform.position = Vector3.MoveTowards(transform.position, target, Time.deltaTime);
     }
+
+    private void SetNewPosition ()
+    {
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        target.x = mousePos.x;
+    }
+
+    #region ArrowsWalk
+    //private void Update()
+    //{
+    //    float input = Input.GetAxis("Horizontal");
+
+    //    if (Mathf.Abs(input)>0.1f)
+    //    {
+    //        Vector2 newPos = new Vector2(transform.position.x + input * speed * Time.deltaTime, transform.position.y);
+    //        transform.position = newPos;
+    //    }
+    //}
+    #endregion
 }
