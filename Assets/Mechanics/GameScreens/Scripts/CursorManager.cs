748e20cc47be3d554b620b7e2f44405f1c12f483
SoundSteppe <sandsteppe@gmail.com>
2022-04-04 01:36:44 +0300

    P13. Добавил изменение курсора при наведении на интерактивные обьекты

---
+++ Assets/Mechanics/GameScreens/Scripts/CursorManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class CursorManager
+{
+    public static CursorManager Instance;
+    private Texture2D _cursorCommon;
+    private Texture2D _cursorHighlight;
+
+    public void Init(Texture2D cursorCommon, Texture2D cursorHighlight)
+    {
+        _cursorHighlight = cursorHighlight;
+        _cursorCommon = cursorCommon;
+        Instance = this;
+        SetCursorHighlight(false);
+    }
+
+    public void SetCursorHighlight(bool highlight)
+    {
+        if(highlight)
+            Cursor.SetCursor(_cursorHighlight, Vector2.zero, CursorMode.ForceSoftware);
+        else
+            Cursor.SetCursor(_cursorCommon, Vector2.zero, CursorMode.ForceSoftware);
+    }
+}
