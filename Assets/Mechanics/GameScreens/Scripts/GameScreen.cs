2d67d7ccda23db4e7ac067f1709aa2344114ec93
Alexander Shishkin <sahanich1@gmail.com>
2022-04-03 17:19:08 +0300

    P7. Кнопки выигрыша и проигрыша

--- Assets/Mechanics/GameScreens/Scripts/GameScreen.cs
+++ Assets/Mechanics/GameScreens/Scripts/GameScreen.cs
@@ -2,23 +2,35 @@
 using UnityEngine;
 using UnityEngine.UI;
 
+public enum GameScreenResult
+{
+    None = 0,
+    WinGame = 1,
+    LoseGame = 2,
+}
+
 public abstract class GameScreen : MonoBehaviour
 {
     [SerializeField]
     private Button CloseButton;
 
+    [SerializeField]
+    private Button CloseWinButton;
+
     public bool IsActive => gameObject.activeSelf;
 
-    public Action CloseRequested;
+    public Action<GameScreenResult> CloseRequested;
 
     private void OnEnable()
     {
-        CloseButton?.onClick.AddListener(() => CloseRequested?.Invoke());
+        CloseButton?.onClick.AddListener(() => CloseRequested?.Invoke(GameScreenResult.LoseGame));
+        CloseWinButton?.onClick.AddListener(() => CloseRequested?.Invoke(GameScreenResult.WinGame));
     }
 
     private void OnDisable()
     {
         CloseButton?.onClick.RemoveAllListeners();
+        CloseWinButton?.onClick.RemoveAllListeners();
     }
 
     public virtual void Show()
