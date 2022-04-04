5ddc5a7f29b73e6a02199ed55369e12b98266296
SniffbI4 <serzh.better@yandex.ru>
2022-04-04 18:08:17 +0300

    Работа с проигрывателем

---
+++ Assets/Mechanics/GameScreens/Scripts/MusicPlayerMiniGameScreen.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class MusicPlayerMiniGameScreen : GameScreen
+{
+    [SerializeField]
+    private GameObject MusicPlayerMiniGamePrefab;
+
+    private GameObject _musicPlayerMiniGameObject;
+
+    public override void Show()
+    {
+        base.Show();
+        if (_musicPlayerMiniGameObject == null)
+        {
+            _musicPlayerMiniGameObject = Instantiate(MusicPlayerMiniGamePrefab, transform);
+           // _musicPlayerMiniGameObject.GetComponent<ChessController>().OnFinish += MusicPlayerMiniGameScreen_OnFinish;
+        }
+    }
+
+    private void MusicPlayerMiniGameScreen_OnFinish()
+    {
+        Debug.Log("MusicPlayerQuest is ended");
+        CloseRequested?.Invoke(GameScreenResult.WinGame);
+    }
+
+    public override void Close()
+    {
+        base.Close();
+    }
+}
