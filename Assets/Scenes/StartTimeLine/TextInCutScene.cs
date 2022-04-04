94529380b91d3eed77b6d3e04dae3f02816e64be
SniffbI4 <serzh.better@yandex.ru>
2022-04-04 22:16:37 +0300

    Начальная катсцена

---
+++ Assets/Scenes/StartTimeLine/TextInCutScene.cs
@@ -0,0 +1,109 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System;
+using SimpleMan.CoroutineExtensions;
+
+[System.Serializable]
+public class Dialogs
+{
+    public string person;
+    public string phrase;
+}
+
+public class TextInCutScene : MonoBehaviour
+{
+    public event Action OnStartCutSceneEnded;
+
+    [SerializeField]
+    private TMP_Text textInCanvas;
+
+    [SerializeField]
+    private Dialogs[] dialogs;
+
+    [SerializeField]
+    private float timeBetweenLetters;
+    [SerializeField]
+    private float timeBetweenPhrases;
+
+    private string currentText;
+    private int currentPhraseIndex = 0;
+    private int currentLetterIndex = 0;
+
+    private bool sceneIsEnded;
+
+    private void Start()
+    {
+    }
+
+    private void OnEnable()
+    {
+        currentText = dialogs[currentPhraseIndex].phrase;
+        ShowCurrentPhrase("");
+        StartCoroutine(LetterShower());
+
+    }
+
+    private void ShowPartOfText (string text)
+    {
+        textInCanvas.text = text;
+    }
+
+    private void ShowCurrentPhrase (string text)
+    {
+        textInCanvas.text = $"{dialogs[currentPhraseIndex].person}: {text}";
+    }
+
+    private void Update()
+    {
+        if (sceneIsEnded)
+            return;
+
+        if (Input.GetMouseButtonDown(0) || currentLetterIndex >= dialogs[currentPhraseIndex].phrase.Length)
+        {
+            StopAllCoroutines();
+
+            StartCoroutine(WaitCoroutine());
+            ShowCurrentPhrase(currentText);
+            currentPhraseIndex++;
+            currentLetterIndex = 0;
+            if (currentPhraseIndex>=dialogs.Length)
+            {
+                sceneIsEnded = true;
+                StopAllCoroutines();
+                this.Delay(2f, () => OnStartCutSceneEnded?.Invoke());
+                this.Delay(3f, () => this.gameObject.SetActive(false));
+                return;
+            }
+            currentText = dialogs[currentPhraseIndex].phrase;
+        }
+    }
+
+    private IEnumerator LetterShower ()
+    {
+        yield return new WaitForSeconds(timeBetweenLetters);
+
+        string resText = textInCanvas.text + currentText[currentLetterIndex];
+        Debug.Log(resText);
+
+        ShowPartOfText(resText);
+        currentLetterIndex++;
+
+        if (currentLetterIndex >= dialogs[currentPhraseIndex].phrase.Length)
+        {
+            StartCoroutine(WaitCoroutine());
+            yield return null;
+        }
+
+        StartCoroutine(LetterShower());
+    }
+
+    private IEnumerator WaitCoroutine ()
+    {
+        yield return new WaitForSeconds(timeBetweenPhrases);
+
+        ShowCurrentPhrase("");
+        StartCoroutine(LetterShower());
+    }
+}
