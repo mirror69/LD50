920acace409bc9180b7163cb23f34c63fc685487
Demionchek <Dpcustom@yandex.ru>
2022-04-07 17:45:45 +0300

    Cutscene on Main And Camera Fix

--- Assets/Mechanics/Main/MainMenuController.cs
+++ Assets/Mechanics/Main/MainMenuController.cs
@@ -53,16 +53,14 @@ public class MainMenuController : MonoBehaviour
 
     private IEnumerator GameStarting()
     {
-        //fadeScreenImage.gameObject.SetActive(true);
-        //MainMenuMusic.DOFade(0, 1);
-        //fadeScreenImage.DOFade(1, 1);
-        //yield return new WaitForSeconds(1);
-        //UIScreenController.HideCurrentScreen();
-        //StartTimeline.Play();
-        //TextInCutScene.gameObject.SetActive(true);
-        //TextInCutScene.OnStartCutSceneEnded += LoadNewGame;
-        yield return null;
-        LoadNewGame();
+        fadeScreenImage.gameObject.SetActive(true);
+        MainMenuMusic.DOFade(0, 1);
+        fadeScreenImage.DOFade(1, 1);
+        yield return new WaitForSeconds(1);
+        UIScreenController.HideCurrentScreen();
+        StartTimeline.Play();
+        TextInCutScene.gameObject.SetActive(true);
+        TextInCutScene.OnStartCutSceneEnded += LoadNewGame;
     }
 
     private void LoadNewGame()
