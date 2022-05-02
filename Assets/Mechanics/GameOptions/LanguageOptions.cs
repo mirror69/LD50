38a2da24d0a8ecfdd9e2624e4247de2abf75954c
Alexander Shishkin <sahanich1@gmail.com>
2022-05-02 13:28:49 +0300

    Добавлена русская локализация

---
+++ Assets/Mechanics/GameOptions/LanguageOptions.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class LanguageOptions
+{
+    public const string LanguageParamName = "Language";
+
+    public string LocaleCode { get; private set; }
+
+    public void SetLocale(string localeCode)
+    {
+        LocaleCode = localeCode;
+        SaveToStorage();
+    }
+
+    public bool LoadFromStorage()
+    {
+        if (PlayerPrefs.HasKey(LanguageParamName))
+        {
+            LocaleCode = PlayerPrefs.GetString(LanguageParamName);
+            return true;
+        }
+        return false;
+    }
+
+    /// <summary>
+    /// Сохранить параметры в хранилище
+    /// </summary>
+    private void SaveToStorage()
+    {
+        PlayerPrefs.SetString(LanguageParamName, LocaleCode);
+    }
+}
